75cde38ff8e5760754416494049583ad4f66eba1
Keshavshetty707 <mkeshav9241217275@gmail.com>
2025-01-06 15:33:58 +0530

    feat(tts): Add speak() function to convert text to speech using pyttsx3

--- Web_app/email_system.py
+++ Web_app/email_system.py
@@ -9,3 +9,28 @@ from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
 from gtts import gTTS
+
+def recognize_speech():
+    recognizer = sr.Recognizer()
+    while True:
+        try:
+            with sr.Microphone() as source:
+                recognizer.adjust_for_ambient_noise(source,duration=0.5)  # Adjust for ambient noise with shorter duration
+                speak("Listening...")
+                audio = recognizer.listen(source, timeout=5)  # Set a timeout for listening
+            text = recognizer.recognize_google(audio, language="en-US", show_all=False)
+            return text
+        except sr.WaitTimeoutError:
+            speak("Listening timeout. Please speak again.")
+        except sr.UnknownValueError:
+            speak("Could not understand audio. Please repeat your command.")
+        except sr.RequestError as e:
+            speak(f"Could not request results; {e}")
+            return None
+
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    
