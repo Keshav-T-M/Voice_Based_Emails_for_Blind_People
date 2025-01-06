f246a81fc624e9f0d2cb6265e04e34d66a1bf212
Keshavshetty707 <mkeshav9241217275@gmail.com>
2025-01-06 15:31:11 +0530

    feat(dependencies): Import libraries for email sending/receiving, MIME handling, speech recognition, and text-to-speech

---
+++ Web_app/email_system.py
@@ -0,0 +1,11 @@
+import imaplib
+import smtplib
+import email
+import re
+import speech_recognition as sr
+import pyttsx3
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from gtts import gTTS
